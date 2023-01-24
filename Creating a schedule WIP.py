4ffa96481a5274d757e9551fe6b03cbe5785e877
Saad Nordine <114657050+Applethal@users.noreply.github.com>
2023-01-25 00:30:18 +0100

    Update Creating a schedule WIP.py

--- Creating a schedule WIP.py
+++ Creating a schedule WIP.py
@@ -11,11 +11,21 @@ for i in range(Number_of_jobs):
     job_dict["Duration"] = Job_duration
     Job_dict[i] = job_dict
 
-######
+###### Makespan
 Makespan = 0
 for i in range(Number_of_jobs):
 
     Schedule.append(Job_dict[i].get("Job"))
     Makespan += Job_dict[i].get("Duration")
 
+##### Weight defaults to 1 unless I update the code to let custome values to be added by the user
+
+Weighted_Makespan = 0
+Weight = 1
+for i in range(Number_of_jobs):
+
+    Weighted_Makespan += Weight * Job_dict[i].get("Duration")
+    Weight -= 0.1
+
 print("The given schedule is", Schedule, "with a makespan of", Makespan)
+print("The weighted completion time of the schedule is ", Weighted_Makespan)
